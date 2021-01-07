7f9010947fea916816dffc77857706b0f3a35bf6
Matthias Asche <matthias@Matthiass-MacBook-Pro.local>
2021-01-07 12:36:17 +0100

    modified program for discrete mathematics

--- src/brutforce.java
+++ src/brutforce.java
@@ -1,26 +1,69 @@
+import java.math.BigInteger;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.concurrent.atomic.AtomicLong;
 
+
+/**
+ * about 3,2 million combinations per second
+ * First password: X42a0 - 48sec
+ * Second password: :( - stopped after 6h -> worst case: 433.665 weeks
+ * Third password: not even tried -> worst case 440775,0351 weeks -> ≈ 765 years
+ *
+ */
 
 public class brutforce {
     private static volatile boolean foundPw;
-    private static ExecutorService executor = null;
+    public static AtomicLong counter = new AtomicLong(0);
+
 
 
     public static void main(String[] args) {
-        String hashedPW = gethashPW(args[0]);
-        System.out.println(hashedPW);
+
         char[] chars = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r',
                 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B',
                 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V',
                 'W', 'X', 'Y', 'Z'};
 
-        hackThisShit(hashedPW, chars);
+        char[] chars2 = {'Z' ,'Y' ,'X' ,'W' ,'V' ,'U' ,'T' ,'S' ,'R' ,'Q' ,'P' ,'O' ,'N' ,'M' ,
+                'L' ,'K' ,'J' ,'I' ,'H' ,'G' ,'F' ,'E' ,'D' ,'C' ,'B' ,'A' ,'9' ,'8' ,'7' ,'6' ,'5'
+                ,'4' ,'3' ,'2' ,'1' ,'0' ,'z' ,'y' ,'x' ,'w' ,'v' ,'u' ,'t' ,'s' ,'r' ,'q' ,'p' ,'o'
+                ,'n' ,'m' ,'l' ,'k' ,'j' ,'i' ,'h' ,'g' ,'f' ,'e' ,'d' ,'c' ,'b' ,'a'};
+
+        char[] chars3 = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9',
+                'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r',
+                's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B',
+                'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V',
+                'W', 'X', 'Y', 'Z'};
+
+        char[] chars4 = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B',
+                'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V',
+                'W', 'X', 'Y', 'Z',
+                'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r',
+                's', 't', 'u', 'v', 'w', 'x', 'y', 'z' };
+
+        char[] chars5 = {'A', 'B',
+                'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V',
+                'W', 'X', 'Y', 'Z',
+                'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r',
+                's', 't', 'u', 'v', 'w', 'x', 'y', 'z', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', };
+
+        char[] chars6 = {'A', 'B',
+                'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V',
+                'W', 'X', 'Y', 'Z','0', '1', '2', '3', '4', '5', '6', '7', '8', '9',
+                'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r',
+                's', 't', 'u', 'v', 'w', 'x', 'y', 'z' };
+
+
+
+        nsaStuff(args[0], chars, chars2, chars3, chars4, chars5, chars6);
+
     }
 
     private static class hackerman implements Runnable {
+
         String pw;
         char[] chars;
         int min;
@@ -42,6 +85,7 @@ public class brutforce {
         }
 
         private static void generate(String current, int len, char[] chars, String hashedPw) {
+
             if (foundPw) {
                 return;
             }
@@ -63,13 +107,16 @@ public class brutforce {
 
     private static String gethashPW(String pw) {
         try {
-            MessageDigest md = MessageDigest.getInstance("SHA-256");
+            MessageDigest md = MessageDigest.getInstance("SHA-1");
             byte[] bytes = md.digest(pw.getBytes());
-            StringBuilder sb = new StringBuilder();
-            for (byte b : bytes) {
-                sb.append(Integer.toString((b & 0xff) + 0x100, 16).substring(1));
+            BigInteger no = new BigInteger(1, bytes);
+            String hashtext = no.toString(16);
+
+            while (hashtext.length() < 32) {
+                hashtext = "0" + hashtext;
             }
-            return sb.toString();
+            return hashtext;
+
         } catch (NoSuchAlgorithmException e) {
             return e.toString();
         }
@@ -81,24 +128,27 @@ public class brutforce {
             System.out.println("Password found: " + pw2);
             return true;
         }
+        counter.incrementAndGet();
         return false;
     }
 
 
-    private static void hackThisShit(String hashedPW, char[] chars) {
-        executor = Executors.newFixedThreadPool(4);
+    private static void nsaStuff(String hashedPW, char[] chars, char[] chars2, char[] chars3, char[] chars4, char[] chars5, char[] chars6) {
+        ExecutorService executor = Executors.newFixedThreadPool(6);
         double startTime = System.nanoTime();
 
-        executor.execute(new hackerman(hashedPW, chars, 1, 3));
-        executor.execute(new hackerman(hashedPW, chars, 4, 5));
-        executor.execute(new hackerman(hashedPW, chars, 5, 6));
-        executor.execute(new hackerman(hashedPW, chars, 6, 7));
+        executor.execute(new hackerman(hashedPW, chars, 5, 5));
+        executor.execute(new hackerman(hashedPW, chars2, 5, 5));
+        executor.execute(new hackerman(hashedPW, chars3, 5, 5));
+        executor.execute(new hackerman(hashedPW, chars4, 5, 5));
+        executor.execute(new hackerman(hashedPW, chars5, 5, 5));
+        executor.execute(new hackerman(hashedPW, chars6, 5, 5));
         while (!foundPw) {
         }
 
 
         executor.shutdownNow();
-        System.out.printf("Time: %f s\n", (System.nanoTime() - startTime) * Math.pow(10, -9));
+        System.out.printf("Time: %f s\nTried %d passwords", (System.nanoTime() - startTime) * Math.pow(10, -9), counter.get());
         System.exit(0);
 
 
